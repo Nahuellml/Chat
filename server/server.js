514faa4dcb6ac6f5221778a50fc6363d9106f0be
Nahuel Alvarez <alvareznahue@gmail.com>
2024-11-27 16:47:52 -0300

    probando path1

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 
 // Ruta para servir `index.html`
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  res.sendFile(path.join(__dirname, 'client/public/index.html'));
 });
 
 
